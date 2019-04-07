0081ac5c19f55bc3c5775b44f2bb7324d488a3ad
Larissa <larissa.teixeira@ufu.br>
2019-04-07 12:50:55 -0300

    space

--- realtors/models.py
+++ realtors/models.py
@@ -10,4 +10,4 @@ class Realtor(models.Model):
     is_mvp = models.BooleanField(default = False) #seller of the month
     hire_date = models.DateTimeField(default = datetime.now, blank = True)
     def __str__(self):
-        return self.name
+        return self.name 
